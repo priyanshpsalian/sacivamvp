efe02d8aad6da09264ac0671e5bb4281b06a5b8f
omkar-murala <omkar.developer1579@gmail.com>
2024-07-27 16:33:21 +0530

    user proffile updated

--- src/app/userprofile/[user]/page.tsx
+++ src/app/userprofile/[user]/page.tsx
@@ -1,15 +1,15 @@
 "use client"
 
-import React, { useState } from 'react';
-import { useParams } from 'next/navigation';
-import { CldUploadButton } from 'next-cloudinary';
 import axios from "axios";
-import { useRouter } from 'next/navigation';
+import { CldUploadButton } from 'next-cloudinary';
+import Image from "next/image";
+import { useParams, useRouter } from 'next/navigation';
+import { useState } from 'react';
 
 function Page() {
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
-  const [univeristy, setUniversity] = useState('');
+  const [university, setUniversity] = useState('');
   const [Language, setLanguage] = useState('');
   const [major, setMajor] = useState('');
   const [country, setCountry] = useState('');
@@ -29,7 +29,6 @@ function Page() {
     throw new Error('Invalid userId type');
   }
 
-
   // upload image
   async function uploadImage(result: any) {
     console.log(result.info.secure_url);
@@ -40,7 +39,7 @@ function Page() {
   async function profileUploaded() {
     try {
       const response = await axios.post("/api/user", {
-        email, name, gender, age: parseInt(age), image: imagelink, university: univeristy, major, language: Language, country
+        email, name, gender, age: parseInt(age), image: imagelink, university, major, language: Language, country
       })
       if (response) {
         Router.push('/buffer')
@@ -48,11 +47,10 @@ function Page() {
     } catch (error: any) {
       console.log(error)
     }
-
   }
 
   return (
-    <div> 
+    <div>
       <div className="mx-auto max-w-7xl px-4">
         <div className="flex flex-col space-y-8 pb-10 pt-12 md:pt-24">
           <div className="mx-auto max-w-max rounded-full border bg-gray-50 p-1 px-3">
@@ -66,7 +64,7 @@ function Page() {
         </div>
         <div className="mx-auto max-w-7xl py-12 md:py-24">
           <div className="grid items-center justify-items-center gap-x-4 gap-y-10 lg:grid-cols-2">
-            {/* contact from */}
+            {/* contact form */}
             <div className="flex items-center justify-center">
               <div className="px-2 md:px-12">
                 <p className="text-2xl font-bold text-gray-900 md:text-4xl">Get in touch</p>
@@ -77,17 +75,19 @@ function Page() {
                 </div>
                 <form action="" className="mt-8 space-y-4">
                   <div className="grid w-full gap-y-4 md:gap-x-4 lg:grid-cols-2">
-                    <div className="grid w-full  items-center gap-1.5">
+                    <div className="grid w-full items-center gap-1.5">
                       <label
                         className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                         htmlFor="phone_number"
                       >
                         Profile Image
                       </label>
-                      <img
+                      <Image
                         className="relative z-0 inline-block h-20 w-20 rounded-full ring-2 ring-white"
                         src={image}
-                        alt="Delba"
+                        alt="Profile"
+                        width={80}
+                        height={80}
                       />
                       <CldUploadButton
                         className="bg-green-400 rounded-full px-6 py-2"
@@ -95,7 +95,7 @@ function Page() {
                         uploadPreset="pf0ysyc8"
                       />
                     </div>
-                    <div className="grid w-full  items-center gap-1.5">
+                    <div className="grid w-full items-center gap-1.5">
                       <label
                         className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                         htmlFor="first_name"
@@ -103,7 +103,7 @@ function Page() {
                         Name
                       </label>
                       <input
-                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 text-gray-900 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                         type="text"
                         id="first_name"
                         placeholder="Name"
@@ -116,7 +116,7 @@ function Page() {
                         Age
                       </label>
                       <input
-                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                        className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 text-gray-900 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                         type="number"
                         id="last_name"
                         placeholder="Age"
@@ -124,7 +124,7 @@ function Page() {
                       />
                     </div>
                   </div>
-                  <div className="grid w-full  items-center gap-1.5">
+                  <div className="grid w-full items-center gap-1.5">
                     <label
                       className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                       htmlFor="email"
@@ -132,14 +132,14 @@ function Page() {
                       University
                     </label>
                     <input
-                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 text-gray-900 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                       type="text"
                       id="university"
                       placeholder="University Name"
                       onChange={(e) => setUniversity(e.target.value)}
                     />
                   </div>
-                  <div className="grid w-full  items-center gap-1.5">
+                  <div className="grid w-full items-center gap-1.5">
                     <label
                       className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                       htmlFor="phone_number"
@@ -147,14 +147,14 @@ function Page() {
                       Language
                     </label>
                     <input
-                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 text-gray-900 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                       type="text"
                       id="language"
                       placeholder="Language"
                       onChange={(e) => setLanguage(e.target.value)}
                     />
                   </div>
-                  <div className="grid w-full  items-center gap-1.5">
+                  <div className="grid w-full items-center gap-1.5">
                     <label
                       className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                       htmlFor="phone_number"
@@ -162,14 +162,14 @@ function Page() {
                       Major
                     </label>
                     <input
-                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 text-gray-900 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                       type="text"
                       id="Major"
                       placeholder="Major"
                       onChange={(e) => setMajor(e.target.value)}
                     />
                   </div>
-                  <div className="grid w-full  items-center gap-1.5">
+                  <div className="grid w-full items-center gap-1.5">
                     <label
                       className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                       htmlFor="phone_number"
@@ -177,21 +177,21 @@ function Page() {
                       Country
                     </label>
                     <input
-                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                      className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 text-gray-900 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                       type="text"
                       id="country"
                       placeholder="Country"
                       onChange={(e) => setCountry(e.target.value)}
                     />
                   </div>
-                  <div className="grid w-full  items-center gap-1.5">
+                  <div className="grid w-full items-center gap-1.5">
                     <label
                       className="text-sm font-medium leading-none text-gray-700 peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                       htmlFor="phone_number"
                     >
                       Gender
                     </label>
-                    <select name="" id="" onChange={(e) => setGender(e.target.value)}>
+                    <select name="" id="" onChange={(e) => setGender(e.target.value)} className="flex h-10 w-full rounded-md border border-gray-300 bg-transparent px-3 py-2 text-sm text-gray-900 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-gray-700 dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900">
                       <option value="Male">Male</option>
                       <option value="Female">Female</option>
                       <option value="NAN">Not Preferred</option>
@@ -207,10 +207,12 @@ function Page() {
                 </form>
               </div>
             </div>
-            <img
+            <Image
               alt="Contact us"
               className="hidden max-h-full w-full rounded-lg object-cover lg:block"
               src="https://images.unsplash.com/photo-1615840287214-7ff58936c4cf?ixlib=rb-4.0.3&auto=format&fit=crop&w=687&h=800&q=80"
+              width={687}
+              height={800}
             />
           </div>
         </div>
